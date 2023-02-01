98fe541fbf490c3af86ba6400dd728b1450badf3
Felipe Oliver <felioliver96@gmail.com>
2023-02-01 12:01:22 +0100

    fix #2 fix #1 functions for building and testing in server done

--- build_test.py
+++ build_test.py
@@ -3,11 +3,11 @@ import subprocess
 def clone_repo(git_repo, branch):
     subprocess.run(["git","clone","-b", branch, git_repo, "../ClonedRepo"])
 
-def install_requirements():
-    pass
+def install_requirements(command):
+    subprocess.run([command])
 
-def build_repo():
-    pass
+def build_repo(command):
+    subprocess.run([command])
 
-def test_repo():
-    pass
+def test_repo(command):
+    subprocess.run([command])
