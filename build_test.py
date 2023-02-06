363869750cca83767556fa40475c52142678c02a
Felipe Oliver <felioliver96@gmail.com>
2023-02-06 11:40:24 +0100

    feat #13 cloned and delete repo successfully

--- build_test.py
+++ build_test.py
@@ -1,37 +1,18 @@
 import subprocess
 import os
-
-repo = "temp"
+import pdb
 
 def clone_repo(git_repo, branch):
-    subprocess.run(["git","clone","-b", branch, "https://github.com/" + git_repo + ".git", "../" + repo])
+    subprocess.run(["git","clone","-b", branch, "https://github.com/" + git_repo + ".git"])
 
 def install_requirements(command):
-    subprocess.run(command)
+    subprocess.run([command])
 
 def build_repo(command):
     subprocess.run([command])
 
-def test_repo(repo):
-   # subprocess.run(["cd", "../" + repo])
-    subprocess.run(["python3", "-m", "pytest", "tests", "../" + repo])
+def repo_test(command):
+    subprocess.run([command])
 
 def delete_repo(repo_name):
-    subprocess.run(["rm","-rf","../"+repo_name])
-
- #https://github.com/OscarLBergstrom/Group-13.git
- 
-clone_repo("OscarLBergstrom/Group-13", "testfest")
-install_requirements(["pip", "install", "-r", "../temp/requirements.txt"])
-test_repo(repo)
-delete_repo(repo)
-
-# try:
-#     clone_repo("git@github.com:OscarLBergstrom/Group-13.git","main")
-#     install_requirements(["pip3","install","-r","requirements.txt"])
-#     response_test = test_repo(["pytest","-s"])
-#     print(response_test)
-#     delete_repo("Group-13")
-# except Exception as e:
-#     print("An error ocurred\n")
-#     print("Error: " + str(e))
+    subprocess.run(["rm","-rf", repo_name])
--- tests/test_CI.py
+++ tests/test_CI.py
@@ -1,7 +1,14 @@
 import pytest
+from pathlib import Path
+from build_test import *
+import pdb
 
-def sample_test(x):
-    return x + 1
+def test_clone_repo():
+    clone_repo("OscarLBergstrom/Group-13", "testfest")
+    path = Path('./Group-13')
+    assert path.exists() == True
 
-def test_sample():
-    assert sample_test(3) == 4
+def test_delete_repo():
+    delete_repo("Group-13")
+    path = Path('Group-13')
+    assert path.exists() == False
