8bb4915c7a45a6bbc1376f7382b574d18e729f3a
Gustaf Johansson <gustajoh@kth.se>
2023-02-06 11:03:07 +0100

    feat: fixes #8

--- build_test.py
+++ build_test.py
@@ -1,7 +1,10 @@
 import subprocess
+import os
+
+repo = "temp"
 
 def clone_repo(git_repo, branch):
-    subprocess.run(["git","clone","-b", branch, git_repo])
+    subprocess.run(["git","clone","-b", branch, "https://github.com/" + git_repo + ".git", "../" + repo])
 
 def install_requirements(command):
     subprocess.run(command)
@@ -9,18 +12,26 @@ def install_requirements(command):
 def build_repo(command):
     subprocess.run([command])
 
-def test_repo(command):
-    subprocess.run(command).returncode
+def test_repo(repo):
+   # subprocess.run(["cd", "../" + repo])
+    subprocess.run(["python3", "-m", "pytest", "tests", "../" + repo])
 
 def delete_repo(repo_name):
-    subprocess.run(["rm","-rf",repo_name])
+    subprocess.run(["rm","-rf","../"+repo_name])
 
-try:
-    clone_repo("git@github.com:OscarLBergstrom/Group-13.git","main")
-    install_requirements(["pip3","install","-r","requirements.txt"])
-    response_test = test_repo(["pytest","-s"])
-    print(response_test)
-    delete_repo("Group-13")
-except Exception as e:
-    print("An error ocurred\n")
-    print("Error: " + str(e))
+ #https://github.com/OscarLBergstrom/Group-13.git
+ 
+clone_repo("OscarLBergstrom/Group-13", "testfest")
+install_requirements(["pip", "install", "-r", "../temp/requirements.txt"])
+test_repo(repo)
+delete_repo(repo)
+
+# try:
+#     clone_repo("git@github.com:OscarLBergstrom/Group-13.git","main")
+#     install_requirements(["pip3","install","-r","requirements.txt"])
+#     response_test = test_repo(["pytest","-s"])
+#     print(response_test)
+#     delete_repo("Group-13")
+# except Exception as e:
+#     print("An error ocurred\n")
+#     print("Error: " + str(e))
