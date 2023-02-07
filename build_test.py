589aa2721bd66e1f183e9ca80987448d4782d061
Gustaf Johansson <gustajoh@kth.se>
2023-02-07 15:22:24 +0100

    feat: Implements automatic cloning, building, testing  #1, #2, #8, #12.
    Co-authored-by: Yatex <felioliver96@gmail.com>

--- build_test.py
+++ build_test.py
@@ -1,18 +1,62 @@
 import subprocess
 import os
 import pdb
+from pathlib import Path
+import yaml
+from yaml.loader import SafeLoader
+
 
 def clone_repo(git_repo, branch):
     subprocess.run(["git","clone","-b", branch, "https://github.com/" + git_repo + ".git"])
 
-def install_requirements(command):
-    subprocess.run([command])
+def install_requirements(command, repo):
+    code = subprocess.run(command + [repo + "/requirements.txt"]).returncode
+    return code
+def run_command(command, repo):
+    return subprocess.run(command + [repo]).returncode
 
-def build_repo(command):
-    subprocess.run([command])
+def repo_test(command, repo):
+    return subprocess.run(command + [repo + "/tests"]).returncode
 
-def repo_test(command):
-    subprocess.run([command])
-
-def delete_repo(repo_name):
-    subprocess.run(["rm","-rf", repo_name])
+def delete_repo(repo):
+    subprocess.run(["rm","-rf", repo])
+    
+def initialization(repo, branch, dir, config_file):
+    path = Path(config_file)
+    if not path.is_file():
+        raise FileNotFoundError("Config file not found!")
+    
+    # Open the file and load the file
+    clone_repo(repo, branch)
+    exitcode = -1
+    with open(config_file) as f:
+        data = yaml.load(f, Loader=SafeLoader)
+        
+        install = data['jobs']['build-and-test']['steps']['requirements']['command']
+        build = data['jobs']['build-and-test']['steps']['build']['command']
+        testing = data['jobs']['build-and-test']['steps']['run']['command']
+        syntax = data['jobs']['build-and-test']['steps']['syntax']['command']
+        
+        if not install == None:
+            install_command = install.split()
+            install_code = install_requirements(install_command, dir)
+        else:
+            raise Exception("Please enter requirements command in config file. ?")
+        if not build == None:
+            build_command = build.split()
+            build_code = run_command(build_command, dir)
+        elif not syntax == None:
+            syntax_command = syntax.split()
+            build_code = run_command(syntax_command, dir)
+        else:
+            raise Exception("Please enter a build or syntax command in config file.")
+        if not testing == None:
+            testing_command = testing.split()
+            test_code = repo_test(testing_command, dir)
+        else:
+            raise Exception("Please enter unit test command in config file.")
+            
+        
+    delete_repo(dir)
+    #pdb.set_trace()
+    return test_code, install_code, build_code
--- tests/test_CI.py
+++ tests/test_CI.py
@@ -5,10 +5,54 @@ import pdb
 
 def test_clone_repo():
     clone_repo("OscarLBergstrom/Group-13", "testfest")
-    path = Path('./Group-13')
+    path = Path('Group-13')
     assert path.exists() == True
+
+def test_installation():
+    code = install_requirements(['pip', 'install', '-r'], "Group-13")
+    assert code == 0
+
+def test_syntax():
+    code = run_command(['python', '-m', 'compileall'], "Group-13")
+    assert code == 0
+    
+def test_testing():
+    code = repo_test(['python', '-m', 'pytest'], "Group-13")
+    assert code == 2
 
 def test_delete_repo():
     delete_repo("Group-13")
     path = Path('Group-13')
     assert path.exists() == False
+
+def test_exception_installation():
+    repo = 'OscarLBergstrom/Group-13'
+    branch = 'testfest'
+    dir = 'Group-13'
+    config_file = 'config_example_noinstall.yml'
+    with pytest.raises(Exception):
+        codes = initialization(repo, branch, dir, config_file)
+    
+def test_exception_unittest():
+    repo = 'OscarLBergstrom/Group-13'
+    branch = 'testfest'
+    dir = 'Group-13'
+    config_file = 'config_example_notest.yml'
+    with pytest.raises(Exception):
+        codes = initialization(repo, branch, dir, config_file)
+
+def test_exception_buildandsyntax():
+    repo = 'OscarLBergstrom/Group-13'
+    branch = 'testfest'
+    dir = 'Group-13'
+    config_file = 'config_example_nobuildandsyntax.yml'
+    with pytest.raises(Exception):
+        codes = initialization(repo, branch, dir, config_file)
+
+def test_no_exception():
+    repo = 'OscarLBergstrom/Group-13'
+    branch = 'testfest'
+    dir = 'Group-13'
+    config_file = 'config_example.yml'
+    codes = initialization(repo, branch, dir, config_file)
+    assert codes == (2, 0, 0)
